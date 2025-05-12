bc75428f45cd6612ffe831b9573d0f10ed86eb6e
CarlosAl21 <g4to101@hotmail.com>
2025-05-12 17:37:53 -0500

    Se elimino una entidad que ya no sirve para nada y se agrego el crud de la empresa

--- src/empresas/dto/create-empresa.dto.ts
+++ src/empresas/dto/create-empresa.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEmpresaDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateEmpresaDto {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string; // Nombre de la empresa
+    
+    @IsString()
+    @IsNotEmpty()
+    ruc: string; // RUC de la empresa
+    
+    @IsString()
+    @IsNotEmpty()
+    direccion: string; // Dirección de la empresa
+
+    @IsString()
+    @IsNotEmpty()
+    telefono: string; // Teléfono de la empresa
+
+    @IsString()
+    @IsNotEmpty()
+    email: string; // Correo electrónico de la empresa
+}
--- src/inventario/dto/update-inventario.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateInventarioDto } from './create-inventario.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class UpdateInventarioDto extends PartialType(CreateInventarioDto) {
-    @IsString()
-    @IsNotEmpty()
-    id_producto: string;
-}
