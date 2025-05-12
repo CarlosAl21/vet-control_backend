cdf1472428f82de95ae9bbd96932b5a4d714242b
CarlosAl21 <g4to101@hotmail.com>
2025-05-12 17:18:49 -0500

    Cambios menores

--- src/proveedores/dto/create-proveedor.dto.ts
+++ src/proveedores/dto/create-proveedor.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProveedoreDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+import { Empresa } from "src/empresas/entities/empresa.entity";
+
+export class CreateProveedoreDto {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string; // Nombre del proveedor
+
+    @IsString()
+    @IsNotEmpty()
+    direccion: string; // Dirección del proveedor
+
+    @IsString()
+    @IsNotEmpty()
+    telefono: string; // Teléfono del proveedor
+
+    @IsString()
+    @IsNotEmpty()
+    email: string; // Correo electrónico del proveedor
+
+    @IsNotEmpty()
+    id_empresa: Empresa; // Identificador de la empresa a la que pertenece el proveedor
+}
